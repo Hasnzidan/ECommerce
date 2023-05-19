3d0eac8661a6440c941698a944990d44f6d6df40
HasnZidan <Zedanh3@gmail.com>
2023-05-19 19:35:19 +0300

    new

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -29,7 +29,7 @@ namespace WebApplication1
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("ConnectionStrings")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
